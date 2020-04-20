e36c218088eb325764881caa323490a0cd653b61
Jan Soendermann <jan.soendermann+git@gmail.com>
2020-04-20 20:43:02 +0100

    Debug

--- backend/server.rb
+++ backend/server.rb
@@ -1,31 +1,36 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra-websocket'
 require 'gm1356'
 
-set :port, 7200
-set :public_folder, 'public'
-set :listeners, []
+class MyApp < Sinatra::Base
+    configure do
+        device = GM1356::Device.new({ filter: 'a', speed: 'f' })
+        device.read do |r|
+            settings.listeners.each do |l| l(r.spl.to_s) end
+        end
 
-# device = GM1356::Device.new({ filter: 'a', speed: 'f' })
-# device.read do |r|
-#     settings.listeners.each do |l| l(r.spl.to_s) end
-# end
+        set :port, 7200
+        set :public_folder, 'public'
+        set :listeners, []
+    end
 
-get "/" do
-    if !request.websocket?
-        redirect '/index.html'
-    else
-        request.websocket do |ws|
-            ws.onopen do
-                settings.listeners << ws.send
-            end
-
-            ws.onclose do
-                settings.listeners.delete(ws.send)
-            end
-        end 
+    get "/" do
+        if !request.websocket?
+            redirect '/index.html'
+        else
+            request.websocket do |ws|
+                ws.onopen do
+                    settings.listeners << ws.send
+                end
+    
+                ws.onclose do
+                    settings.listeners.delete(ws.send)
+                end
+            end 
+        end
     end
 end
 
+run MyApp.run!
